c0ae829485da972495d9ff77c1991b7efbf1f216
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2023-05-31 04:52:35 +0530

    MongooseModule added

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DiscordMembershipModule } from './discord-membership/discord-membership.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [DiscordMembershipModule],
+  imports: [DiscordMembershipModule,
+    MongooseModule.forRoot(
+      process.env['MONGO_ATLAS_URL'] as string
+    )
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
