811001d1639e0c8c25596dd7e5710e566f591bd9
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2023-06-02 16:51:50 +0530

    reformatted according to prettier

--- apps/backend/src/discord-membership/routes/nft-studio/nft-studio.service.ts
+++ apps/backend/src/discord-membership/routes/nft-studio/nft-studio.service.ts
@@ -1,59 +1,91 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model} from 'mongoose';
-import { NFTStudioDto, NFTStudioGuildDto } from 'src/discord-membership/dto/nft-studio/index.dto';
-import { NFTStudioGuild, NFTStudioGuildDocument } from 'src/discord-membership/schemas/nft-studio/nft-studio-guild.schema';
+import { Model } from 'mongoose';
+import {
+    NFTStudioDto,
+    NFTStudioGuildDto,
+} from 'src/discord-membership/dto/nft-studio/index.dto';
+import {
+    NFTStudioGuild,
+    NFTStudioGuildDocument,
+} from 'src/discord-membership/schemas/nft-studio/nft-studio-guild.schema';
 import { NFTStudioPublicKey } from 'src/discord-membership/schemas/nft-studio/nft-studio-public-key.schema';
 import { NFTStudio } from 'src/discord-membership/schemas/nft-studio/nft-studio.schema';
-import {GuildObject, getGuild, getGuildBannerURL, getGuildIconURL} from '@excluence-repo/discord-connector';
+import {
+    GuildObject,
+    getGuild,
+    getGuildBannerURL,
+    getGuildIconURL,
+} from '@excluence-repo/discord-connector';
 
-
-type _NFTStudioGuildDocument = NFTStudioGuildDocument & {studio: string};
+type _NFTStudioGuildDocument = NFTStudioGuildDocument & { studio: string };
 
 @Injectable()
 export class NftStudioService {
     constructor(
         @InjectModel(NFTStudio.name) private nftStudioModel: Model<NFTStudio>,
-        @InjectModel(NFTStudioPublicKey.name) 
-            private nftStudioPublicModel: Model<NFTStudioPublicKey>,
+        @InjectModel(NFTStudioPublicKey.name)
+        private nftStudioPublicModel: Model<NFTStudioPublicKey>,
         @InjectModel(NFTStudioGuild.name)
-            private nftStudioGuildModel: Model<NFTStudioGuild>
-    ){}
+        private nftStudioGuildModel: Model<NFTStudioGuild>,
+    ) {}
 
-    private _serializeNFTStudioGuild(nftStudioGuild: _NFTStudioGuildDocument, guild: GuildObject): NFTStudioGuildDto {
-
+    private _serializeNFTStudioGuild(
+        nftStudioGuild: _NFTStudioGuildDocument,
+        guild: GuildObject,
+    ): NFTStudioGuildDto {
         return {
             studio_id: nftStudioGuild.studio,
             guildId: nftStudioGuild.guildId,
             name: guild.name,
-            banner: guild.banner !== null? getGuildBannerURL(guild.id, guild.banner): null,
-            icon: guild.icon !== null? getGuildIconURL(guild.id, guild.icon): null
+            banner:
+                guild.banner !== null
+                    ? getGuildBannerURL(guild.id, guild.banner)
+                    : null,
+            icon:
+                guild.icon !== null
+                    ? getGuildIconURL(guild.id, guild.icon)
+                    : null,
         };
     }
 
-
-    private async serializeNFTStudioGuilds(studioGuilds: _NFTStudioGuildDocument[]): Promise<NFTStudioGuildDto[]> {
+    private async serializeNFTStudioGuilds(
+        studioGuilds: _NFTStudioGuildDocument[],
+    ): Promise<NFTStudioGuildDto[]> {
         const guilds: Record<string, GuildObject> = {};
         return await Promise.all(
-            studioGuilds.map(async(studioGuild) => {
+            studioGuilds.map(async (studioGuild) => {
                 if (guilds[studioGuild.guildId] === undefined) {
-                    guilds[studioGuild.guildId] = await getGuild({id: studioGuild.guildId, with_counts: false});
+                    guilds[studioGuild.guildId] = await getGuild({
+                        id: studioGuild.guildId,
+                        with_counts: false,
+                    });
                 }
-                return await this._serializeNFTStudioGuild(studioGuild, guilds[studioGuild.guildId]);
-            })
+                return await this._serializeNFTStudioGuild(
+                    studioGuild,
+                    guilds[studioGuild.guildId],
+                );
+            }),
         );
     }
 
- 
     async createNFTStudio(nftStudioDto: NFTStudioDto): Promise<NFTStudio> {
         const nftStudio = new this.nftStudioModel(nftStudioDto);
         return await nftStudio.save();
     }
 
-    async findNFTStudioGuildByGuildId(guildId: string): Promise<NFTStudioGuildDto | null> {
-        const nftStudioGuild =  await this.nftStudioGuildModel.findOne({guildId: guildId})
-            .select('guildId studio').exec()
+    async findNFTStudioGuildByGuildId(
+        guildId: string,
+    ): Promise<NFTStudioGuildDto | null> {
+        const nftStudioGuild = await this.nftStudioGuildModel
+            .findOne({ guildId: guildId })
+            .select('guildId studio')
+            .exec();
         if (nftStudioGuild === null) return null;
-        return (await this.serializeNFTStudioGuilds([nftStudioGuild as _NFTStudioGuildDocument]))[0];
+        return (
+            await this.serializeNFTStudioGuilds([
+                nftStudioGuild as _NFTStudioGuildDocument,
+            ])
+        )[0];
     }
 }
