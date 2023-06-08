79020f2d688418a7443ef63d3a8ab2c8a1a9f35a
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2023-06-08 23:55:52 +0530

    dotenv added to custom commands

--- apps/backend/src/auth/auth.strategy.ts
+++ apps/backend/src/auth/auth.strategy.ts
@@ -5,14 +5,10 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
     constructor() {
-        console.log('key ', process.env['JWT_ENC_KEY'] as string);
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            // secretOrKey: process.env['JWT_ENC_KEY'] as string,
-            // secret: process.env['JWT_ENC_KEY'] as string,
-            secretOrKey:
-                '255FBE20A909FB602B2D34F56FE20CB0105058E8DE401FD2D55330980D4EC55E',
+            secretOrKey: process.env['JWT_ENC_KEY'] as string,
         });
     }
 
