e7fa26d543668e3f357529c00510c96658a8843f
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2023-06-09 18:10:31 +0530

    removed backend nestjs app

--- apps/backend/src/auth/auth.decorator.ts
+++
@@ -1,8 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-export const IS_PUBLIC_KEY = 'isPublic';
-export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
-
-export const ROLE_KEY = 'role';
-export const IsCreator = () => SetMetadata(ROLE_KEY, 'isCreator');
-export const IsSuperUser = () => SetMetadata(ROLE_KEY, 'isSuperUser');
--- apps/backend/src/nft-studio/nft-studio.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('nft-studio')
-export class NftStudioController {}
